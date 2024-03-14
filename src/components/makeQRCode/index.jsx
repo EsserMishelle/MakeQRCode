a79fbb922173b3547e80928957c9eb4e5c2b7de2
Esser, Mishelle <EsserMishelle@gmail.com>
2024-03-14 09:33:24 -0400

    first commit

---
+++ src/components/makeQRCode/index.jsx
@@ -0,0 +1,32 @@
+// In MakeQRCode.jsx
+import { useState } from "react";
+import QRCode from "react-qr-code";
+
+export default function MakeQRCode() {
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  function handleMakeQrCode() {
+    setQrCode(input);
+    setInput("");
+  }
+
+  return (
+    <div>
+      <h1 style={{ justifyContent: "center" }}>Make QR Code</h1>
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        value={input}
+        placeholder="Enter a name or URL"
+      />
+
+      <button disabled={!input.trim()} onClick={handleMakeQrCode}>
+        Make Qr Code
+      </button>
+      <br />
+      <br />
+      {qrCode && <QRCode value={qrCode} size={400} bgColor="#fff" />}
+    </div>
+  );
+}
